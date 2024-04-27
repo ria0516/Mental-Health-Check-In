f682386c4150f95731dd019cd83a11eb23773210
Ria Maria Mathew <riamariamathew@Rias-MacBook-Pro-2.local>
2024-04-27 10:50:12 -0700

    added 1 + 2

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    cout << "hello git" << endl;
+    cout << "git is east " << endl;
+    cout << 1 + 2 << endl;
+    cout << "How are you feeling?" << endl;
+    return 0;
+}
